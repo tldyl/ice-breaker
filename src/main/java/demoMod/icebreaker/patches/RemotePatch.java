8d8b5bcf8526e98fb65192ecb883432cb9e6e00e
tldyl <756560020@qq.com>
2022-11-20 23:43:05 +0800

    修复远程不对荆棘生效的bug

--- src/main/java/demoMod/icebreaker/patches/RemotePatch.java
+++ src/main/java/demoMod/icebreaker/patches/RemotePatch.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.FlightPower;
 import com.megacrit.cardcrawl.powers.ThornsPower;
@@ -25,6 +26,19 @@ public class RemotePatch {
                 if (card.hasTag(CardTagEnum.REMOTE) && type == DamageInfo.DamageType.NORMAL) {
                     return SpireReturn.Return(damage);
                 }
+            }
+            return SpireReturn.Continue();
+        }
+    }
+
+    @SpirePatch(
+            clz = ThornsPower.class,
+            method = "onAttacked"
+    )
+    public static class PatchOnAttacked {
+        public static SpireReturn<Integer> Prefix(AbstractPower power, DamageInfo info, int damageAmount) {
+            if (AbstractDungeon.actionManager.cardsPlayedThisCombat.get(AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - 1).hasTag(CardTagEnum.REMOTE)) {
+                return SpireReturn.Return(damageAmount);
             }
             return SpireReturn.Continue();
         }
